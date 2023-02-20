d1a109dbba97c9109bf769c1a598f3ec7759d93e
Sebastian Rojas <sebas.2919.rojas@gmail.com>
2023-02-19 21:46:49 -0500

    Se sube el proyecto

---
+++ src/components/EmptyTodos/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EmptyTodos(){
+    return <p>¡Crear tu primer TODO!</p>
+}
+
+export { EmptyTodos };
---
+++ src/components/TodoSearch/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./TodoSearch.css"
+
+function TodoSearch({ searchValue, setSearchValue }) {
+    const onSearchValueChange = (event) => {
+        setSearchValue(event.target.value);
+    };
+
+    return (
+        <input
+            className="TodoSearch"
+            placeholder="Buscar"
+            value={searchValue}
+            onChange={onSearchValueChange}
+        />
+    );
+}
+
+export { TodoSearch };
